e33b19b522dbaa6f322fa8919cb67ea6ea6b8504
hossamAhmedSalah <100955137+hossamAhmedSalah@users.noreply.github.com>
2022-10-18 20:47:20 +0200

    Add files via upload

--- Stack/stack.h
+++ Stack/stack.h
@@ -5,21 +5,23 @@
 #define MAX 5
 
 typedef int type;
-//typedef struct
-//{
-//  int top;
-//  type elements[MAX];
-//}Stack;
+
 
 void createStack(Stack *s);
 int push(Stack *s, type value);
 type pop(Stack *s);
 //default peeking for the last element
+//see the last element added to the stack
 type peekNorm(Stack*s);
+//peek with a specific index reverse index as the stack is FILO
 type peek(Stack*s, int index_peeking);
+// 1==>Full 0==>not Full
 int isStackFull(Stack s);
+// 1==>Empty 0==>not Empty
 int isStackEmpety(Stack s);
+// return the length of the stack
 int stackLen(Stack *s);
+//not finished and have some issues
 int stackWithout(Stack *s, int index, type*cop);
 
 
